d157f9ee5c7cb3b2663d9e7f36da23136a586460
YagoGG <yagogonzalezg@gmail.com>
2015-01-04 15:53:00 +0100

    Succesfully integrated multithreading
    Ready for merging

--- Ping3r/Form1.Designer.cs
+++ Ping3r/Form1.Designer.cs
@@ -32,7 +32,7 @@
             this.lb_netstatus = new System.Windows.Forms.Label();
             this.bt_run = new System.Windows.Forms.Button();
             this.pb_progress = new System.Windows.Forms.ProgressBar();
-            this.tb_test = new System.Windows.Forms.TextBox();
+            this.tv_results = new System.Windows.Forms.TreeView();
             this.SuspendLayout();
             // 
             // lb_netstatusTitle
@@ -50,9 +50,9 @@
             this.lb_netstatus.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.lb_netstatus.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lb_netstatus.ForeColor = System.Drawing.Color.Gray;
-            this.lb_netstatus.Location = new System.Drawing.Point(382, 22);
+            this.lb_netstatus.Location = new System.Drawing.Point(392, 22);
             this.lb_netstatus.Name = "lb_netstatus";
-            this.lb_netstatus.Size = new System.Drawing.Size(135, 13);
+            this.lb_netstatus.Size = new System.Drawing.Size(125, 13);
             this.lb_netstatus.TabIndex = 3;
             this.lb_netstatus.Text = "undefined";
             this.lb_netstatus.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
@@ -75,24 +75,26 @@
             this.pb_progress.Size = new System.Drawing.Size(266, 23);
             this.pb_progress.TabIndex = 5;
             // 
-            // tb_test
+            // tv_results
             // 
-            this.tb_test.Location = new System.Drawing.Point(165, 103);
-            this.tb_test.Name = "tb_test";
-            this.tb_test.Size = new System.Drawing.Size(100, 20);
-            this.tb_test.TabIndex = 6;
+            this.tv_results.Location = new System.Drawing.Point(21, 76);
+            this.tv_results.Name = "tv_results";
+            this.tv_results.Size = new System.Drawing.Size(496, 122);
+            this.tv_results.TabIndex = 6;
             // 
             // MainForm
             // 
+            this.AcceptButton = this.bt_run;
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoSize = true;
-            this.ClientSize = new System.Drawing.Size(529, 210);
-            this.Controls.Add(this.tb_test);
+            this.ClientSize = new System.Drawing.Size(540, 225);
+            this.Controls.Add(this.tv_results);
             this.Controls.Add(this.pb_progress);
             this.Controls.Add(this.bt_run);
             this.Controls.Add(this.lb_netstatus);
             this.Controls.Add(this.lb_netstatusTitle);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Name = "MainForm";
             this.Text = "Ping3r";
             this.Load += new System.EventHandler(this.Form1_Load);
@@ -107,7 +109,7 @@
         private System.Windows.Forms.Label lb_netstatus;
         private System.Windows.Forms.Button bt_run;
         private System.Windows.Forms.ProgressBar pb_progress;
-        private System.Windows.Forms.TextBox tb_test;
+        public System.Windows.Forms.TreeView tv_results;
     }
 }
 
--- Ping3r/Form1.cs
+++ Ping3r/Form1.cs
@@ -8,7 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-// For ping purposes
+// For managing IP addresses and sending pings
 using System.Net;
 using System.Net.NetworkInformation;
 
@@ -22,9 +22,6 @@ namespace WindowsFormsApplication2
 
     public partial class MainForm : Form 
     {
-        byte i = 0; // IP loop variable
-        Device[] netDevices = new Device[255];
-
         public MainForm() 
         {
             InitializeComponent();
@@ -43,64 +40,55 @@ namespace WindowsFormsApplication2
                 lb_netstatus.Text = "DOWN! (no network detected)";
                 lb_netstatus.ForeColor = Color.Red;
             }
-            ImageList imglist = new ImageList();
-            imglist.Images.Add(Image.FromFile("nodeimg.png"));
         }
 
-        class Device
+        public class Device
         {
-            public IPAddress ip;
+            public String ip;
+            public String hostName = null;
+            public bool available = false;
+            public Ping pingSender = new Ping();
+            public PingReply reply;
 
-            Ping pingSender = new Ping();
-            PingReply pingReply;
-
-            public Device(string inIP)
+            public void IsUP() // Pings this device, to know if it's up or down
             {
-                ip = IPAddress.Parse(inIP); 
-            }
-            // Displays a message box with device's IP address
-            public void ShowIP()    // DEBUG
-            {
-                MessageBox.Show(ip.ToString());
-            }
-            // Checks if device is active
-            public bool IsUp()
-            {
-                pingSender.Send(ip, 2000);
-                if (pingReply.Status == IPStatus.Success)
+                reply = pingSender.Send(ip, 2000);
+                if (reply.Status == IPStatus.Success)
                 {
-                    return true;
+                    available = true;
                 }
-                else
-                {
-                    return false;
-                }
-            }
-        }
 
-        private void CheckDevice()
-        {
-            netDevices[i] = new Device("192.168.1." + i);
-
-            if (netDevices[i].IsUp())
-            {
-                MessageBox.Show(netDevices[i].ip.ToString() + " is currently UP", "Device up");
+                // Memory releasing
+                pingSender = null;
+                reply = null;
+                GC.Collect();
             }
         }
 
         private void bt_run_Click(object sender, EventArgs e)
         {
             double progress = 0;    // Progress bar variable
+            Device[,] netDevices = new Device[255, 255];  // Network devices array
 
-            for (i = 0; i <= 254; i++)
+            for (byte net = 0; net <= 254; net++)
             {
-                Thread devThread = new Thread(new ThreadStart(CheckDevice));
-                devThread.Start();
+                for (byte i = 0; i <= 254; i++)
+                {
+                    netDevices[net, i] = new Device();
 
+                    netDevices[net, i].ip = "192.168." + net + "." + i;
+
+                    Debug.Write("Pinging device 192.168." + net + "." + i + "...\n");
+
+                    /*  MULTITHREADING SYSTEM  */
+                    Thread devThread = new Thread(new ThreadStart(netDevices[net, i].IsUP));
+                    devThread.Start();
+                }
+                // Progress bar advance
                 progress += 0.39;
                 pb_progress.Value = (int)Math.Round(progress);
             }
-            pb_progress.Value = 100;
+            pb_progress.Value = 100;    // Set progress bar to full (because of precission loss on 0.39 additions)
         }
     }
 }
